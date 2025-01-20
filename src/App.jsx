169c37008e372e4fb38d3d0bdd267495fec4076a
ViniciusBenevidesDaSilva <vinishrafiel@gmail.com>
2025-01-20 14:49:05 -0300

    feat: Create product list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Products from './components/Products';
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Products />
     </div>
   );
 }
